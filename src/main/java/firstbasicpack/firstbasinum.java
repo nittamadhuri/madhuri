b9ece0c4d8d9f1b1d456b5efb4c8e12cc4dcc6f4
nittamadhuri <nittamadhuri@gmail.com>
2024-08-02 11:47:14 -0500

    First push of the code

---
+++ src/main/java/firstbasicpack/firstbasinum.java
@@ -0,0 +1,9 @@
+void main(){
+    System.out.println("first Program");
+    int a =10;
+    System.out.println("add tow number" + a +" resul" + a + "is " + (a+a));
+    char c=' ';
+    System.out.println(Character.isWhitespace(c));
+    System.out.println(Character.isLowerCase(c));
+
+}
